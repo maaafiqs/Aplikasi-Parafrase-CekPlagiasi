be2c813ea8fc60012bb8f38ea5d2f78cad3d6430
maaafiqs <syafiqyusuf0308@gmail.com>
2026-09-11 16:52:34 +0800

    fix: fix Vercel deployment - add composer install to build, fix session driver for serverless

--- api/index.php
+++ api/index.php
@@ -6,7 +6,8 @@ $appKey = getenv('APP_KEY') ?: ($_ENV['APP_KEY'] ?? 'base64:z8LAlxHo9ufEoZgYKyVr
 putenv('VERCEL=1');
 putenv("APP_KEY={$appKey}");
 putenv('APP_ENV=production');
-putenv('SESSION_DRIVER=file');
+putenv('APP_DEBUG=false');
+putenv('SESSION_DRIVER=array');
 putenv('CACHE_STORE=array');
 putenv('CACHE_DRIVER=array');
 putenv('QUEUE_CONNECTION=sync');
@@ -16,7 +17,8 @@ putenv('VIEW_COMPILED_PATH=/tmp/views');
 $_ENV['VERCEL'] = '1';
 $_ENV['APP_KEY'] = $appKey;
 $_ENV['APP_ENV'] = 'production';
-$_ENV['SESSION_DRIVER'] = 'file';
+$_ENV['APP_DEBUG'] = 'false';
+$_ENV['SESSION_DRIVER'] = 'array';
 $_ENV['CACHE_STORE'] = 'array';
 $_ENV['CACHE_DRIVER'] = 'array';
 $_ENV['QUEUE_CONNECTION'] = 'sync';
@@ -25,7 +27,7 @@ $_ENV['VIEW_COMPILED_PATH'] = '/tmp/views';
 
 $_SERVER['VERCEL'] = '1';
 $_SERVER['APP_KEY'] = $appKey;
-$_SERVER['SESSION_DRIVER'] = 'file';
+$_SERVER['SESSION_DRIVER'] = 'array';
 $_SERVER['LOG_CHANNEL'] = 'stderr';
 
 // Ensure writable storage and views directories exist in /tmp for Vercel serverless environment
